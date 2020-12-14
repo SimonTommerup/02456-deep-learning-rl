9171f05a2dda1cc7ed4ac42600cccac38c4602bb
Simon Ankjær Tommerup <sito@ihfood.dk>
2020-12-14 13:59:35 +0100

    new name on videos

--- saliency-experiments/initial.py
+++ saliency-experiments/initial.py
@@ -1,100 +1,113 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import utils
-from utils import make_env, Storage, orthogonal_init
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
+import gym
 import procgen
-
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
-
-
-class Encoder(nn.Module):
-  def __init__(self, in_channels, feature_dim):
-    super().__init__()
-    self.layers = nn.Sequential(
-        nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=8, stride=4), nn.ReLU(),
-        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2), nn.ReLU(),
-        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1), nn.ReLU(),
-        Flatten(),
-        nn.Linear(in_features=1024, out_features=feature_dim), nn.ReLU()
-    )
-    self.apply(orthogonal_init)
-
-  def forward(self, x):
-    return self.layers(x)
+import utils
+import initial
+import torch
+import imageio
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+from torchvision.transforms import Resize
+import torch.nn.functional as F
+import cv2
+from tqdm import tqdm
+import models
+import matplotlib.pyplot as plt
 
 
-class Policy(nn.Module):
-  def __init__(self, encoder, feature_dim, num_actions):
-    super().__init__()
-    self.encoder = encoder
-    self.policy = orthogonal_init(nn.Linear(feature_dim, num_actions), gain=.01)
-    self.value = orthogonal_init(nn.Linear(feature_dim, 1), gain=1.)
 
-  def act(self, x):
-    with torch.no_grad():
-      x = x.cuda().contiguous()
-      dist, value = self.forward(x)
-      action = dist.sample()
-      log_prob = dist.log_prob(action)
-    
-    return action.cpu(), log_prob.cpu(), value.cpu()
+def get_mask(center, size, r):
+    y,x = np.ogrid[-center[0]:size[0]-center[0], -center[1]:size[1]-center[1]]
+    keep = x*x + y*y <= 1
+    mask = np.zeros(size) 
+    mask[keep] = 1 # select a circle of pixels
+    mask = gaussian_filter(mask, sigma=r) # blur the circle of pixels. this is a 2D Gaussian for r=r^2=1
+    return mask/mask.max()
 
-  def forward(self, x):
-    x = self.encoder(x)
-    logits = self.policy(x)
-    value = self.value(x).squeeze(1)
-    dist = torch.distributions.Categorical(logits=logits)
+def channel_to_numpy(tensor):
+    size = tensor.shape[0]
+    ndarr = tensor.numpy()
+    ndarr = np.reshape(ndarr, (size,size))
+    return ndarr
 
-    return dist, value
+def channel_to_tensor(ndarr):
+    size = ndarr.shape[0]
+    tensor = torch.from_numpy(ndarr)
+    tensor = tensor.view(1,1,size,size)
+    return tensor
 
+def gaussian_blur(frame, mask):
+    gonc = []
+    goncmask = []
+    for idx,_ in enumerate(frame):
+        channel = frame[0][idx]
+        channel = channel_to_numpy(channel)
+        channel = channel * (np.ones((64,64)) - mask) + gaussian_filter(channel, sigma=3)*mask
+        gonc.append(gaussian_filter(channel, sigma=3))
+        goncmask.append(gaussian_filter(channel, sigma=3)*mask)
+        channel = channel_to_tensor(channel)
+        frame[0][idx] = channel
+    return frame, gonc, goncmask
+
+i = 0
+j = 0
+
+mask = get_mask(center=[32,32], size=[64,64], r=5)
 num_envs = 1
 num_levels = 1
 num_features = 256 
 use_backgrounds=False
+env = utils.make_env(num_envs, env_name="starpilot", start_level=num_levels, num_levels=num_levels, use_backgrounds=use_backgrounds)
+obs = env.reset()
+
+def saliency_frame(frame, pixel_step):
+    ps = pixel_step
+    bfs = []
+    for idx,_ in enumerate(frame[0]):
+        inner_bfs = []
+        for i in range(0, frame.shape[2], ps):
+            for j in range(0, frame.shape[3], ps):
+                mask = get_mask(center=[i,j], size=[64,64], r=5)
+                blurred_frame, g, gm = gaussian_blur(frame.clone(), mask)
+                inner_bfs.append(blurred_frame)
+        bfs.append(inner_bfs)
+    return bfs
 
 
-class PolicyHook():
-  def __init__(self, net, mode):
-    self.name = mode
-    self.module = net._modules[self.name]
-    self.out = {}
-    self._rgsthk(self.module)
+bfs = saliency_frame(obs, pixel_step=4)
 
-  def _rgsthk(self, module):
-    self.module.register_forward_hook(self.hook_fn)
+#%%
 
-  def hook_fn(self, module, input, output):
-    self.out[self.name] = output
 
-  def get_logits(self):
-    return self.out[self.name][0]
+print(type(bfs[0][0]))
 
-if __name__ == "__main__":
-      env = utils.make_env(num_envs, env_name="starpilot", start_level=num_levels, num_levels=num_levels, use_backgrounds=use_backgrounds)
-      obs = env.reset()
+a = torch.stack(bfs[0])
 
-      encoder = Encoder(env.observation_space.shape[0], num_features)
-      policy = Policy(encoder, num_features, env.action_space.n)
-      policy.cuda()
-      policy.eval()
-      phook = PolicyHook(policy, mode="policy")
-      vhook = PolicyHook(policy, mode="value")
 
-      # Use policy on observation on frame
-      action,_,_ = policy.act(obs)
 
-      # Get logits
-      logits = phook.get_logits()
-      value = vhook.get_logits()
 
-      print(logits)
-      print("LenLogits:", len(logits))
 
-      print(value)
-      print("LenLogits:", len(value))
-      
+
+
+# %%
+
+a.shape
+# %%
+
+a[0][0].shape
+
+
+
+# %%
+plt.imshow(img[0])
+plt.imshow(img[1])
+plt.imshow(img[2])
+plt.show()
+# %%
+for img in a:
+  img = img[0][0].numpy()
+  plt.imshow(img[0], cmap=plt.get_cmap("magma"))
+  plt.imshow(img[1], cmap=plt.get_cmap("magma"))
+  plt.imshow(img[2], cmap=plt.get_cmap("magma"))
+  plt.show()
+
+# %%
